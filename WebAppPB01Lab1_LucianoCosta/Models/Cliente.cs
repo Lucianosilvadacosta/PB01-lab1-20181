7dfb238309c2cc63a963261acb0f081c8f2dd9f0
luciano silva da costa maninho <luciano.silvadacosta@outlook.com>
2018-04-16 20:34:37 -0400

    Add project files.

---
+++ WebAppPB01Lab1_LucianoCosta/Models/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppPB01Lab1_LucianoCosta.Models
+{
+    public class Cliente
+    {
+        // Primary key
+        public int ClienteId { get; set; }
+
+        [Required (ErrorMessage = "Nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "CPF é obrigatório")]
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "Email é obrigatório")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Data de cadastro é obrigatório")]
+        public DateTime DataCadastro { get; set; }
+    }
+}
---
+++ WebAppPB01Lab1_LucianoCosta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppPB01Lab1_LucianoCosta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
